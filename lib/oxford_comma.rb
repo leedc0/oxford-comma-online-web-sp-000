74829e5872187c09470a0a39269a7f893e7ca556
Daniel Lee <leedc@me.com>
2019-07-21 22:22:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length = 1
+    return array
+  elsif array.length = 2
+    return array.join( " and ")
+  elsif array.length = 3
+    return array.map { | x | x.to_s }.join(" , ")
+  else array.length > 3
+    return array.map { | x | x.to_s }.join(" , ")
 end
