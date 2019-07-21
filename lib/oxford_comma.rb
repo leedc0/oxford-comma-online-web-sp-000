77b44ffd05e654dd5226913a4af4a1b363733802
Daniel Lee <leedc@me.com>
2019-07-21 22:49:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,14 @@
 def oxford_comma(array)
   array = []
-  if array.length = 1
+  # if array.length = 1
+  #   return array
+  # elsif array.length = 2
+  #   return array.join( " and ")
+  # elsif array.length = 3
+  #   return array.map { | x | x.to_s }.join(" , ")
+  # else array.length > 3
+  #   return array.map { | x | x.to_s }.join(" , ")
+  if array.uniq.count = 1
     return array
   elsif array.length = 2
     return array.join( " and ")
